c2fea2f35bc1c2e81401120992ca6f048b21f741
Overwrite987 <artem.korolev_2003@mail.ru>
2024-08-05 22:20:24 +0300

    make plugin logger static
    so other plugins will be able to use it

--- src/main/java/ru/overwrite/api/OvApi.java
+++ src/main/java/ru/overwrite/api/OvApi.java
@@ -8,7 +8,7 @@ import ru.overwrite.api.logging.PaperLogger;
 
 public final class OvApi extends JavaPlugin {
 
-    private final Logger pluginLogger = VersionUtils.FOLIA ? new PaperLogger() : new BukkitLogger();
+    private static final Logger pluginLogger = VersionUtils.FOLIA ? new PaperLogger() : new BukkitLogger();
 
     @Override
     public void onEnable() {
