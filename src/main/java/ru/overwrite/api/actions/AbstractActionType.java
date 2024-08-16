2e2583887fe1554438a785ba2fa67731de49d58c
Overwrite987 <artem.korolev_2003@mail.ru>
2024-08-16 20:29:10 +0300

    Update AbstractActionType.java

--- src/main/java/ru/overwrite/api/actions/AbstractActionType.java
+++ src/main/java/ru/overwrite/api/actions/AbstractActionType.java
@@ -2,12 +2,30 @@ package ru.overwrite.api.actions;
 
 import net.kyori.adventure.key.Key;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public class AbstractActionType {
+public abstract class AbstractActionType implements ActionType {
 
     protected final Key KEY;
 
     public AbstractActionType(JavaPlugin plugin, String type) {
         KEY = Key.key(plugin.getName() + ":" + type);
+    }
+
+    @Override
+    public @NotNull Action instance(@NotNull String context, @NotNull JavaPlugin plugin) {
+        return new AbstractAction(context);
+    }
+
+    @Override
+    public @NotNull Key key() {
+        return KEY;
+    }
+
+    private record AbstractAction(@NotNull String command) implements Action {
+        @Override
+        public void perform(@NotNull Object... params) {
+            // do nothing
+        }
     }
 }
